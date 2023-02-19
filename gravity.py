43a61a6cde04fb2ce08816194f58f2147a33c7c9
georgeSEC1 <111563036+georgeSEC1@users.noreply.github.com>
2023-02-19 13:35:16 +1100

    Use qpython, for Android...

--- gravity.py
+++ gravity.py
@@ -18,7 +18,7 @@ while(True):
     m+=1
     if var[2] is not None and var[2] == 9.9375:       
         n+=1
-        if m >= 10:
+        if m > 10:
             m = 0
             print(n)
             if n >= 11:
